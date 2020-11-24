c407b3fd56d111db96c296f8cafcfc60f6da2b80
Nemanja Vicovic <nemanja.vicovic@ftn.edu.rs>
2020-11-24 20:06:45 +0100

    Napravljena web aplikacija i prikaz studenata

---
+++ PresentationLayer/PresentationLayerWeb/Default.aspx.cs
@@ -0,0 +1,29 @@
+﻿using BusinessLayer;
+using DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PresentationLayerWeb
+{
+    public partial class _Default : Page
+    {
+        private StudentBusiness studentBusiness;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            this.studentBusiness = new StudentBusiness();
+
+            List<Student> students = this.studentBusiness.GetAllStudents();
+            listBoxStudents.Items.Clear();
+
+            foreach (Student s in students)
+            {
+                listBoxStudents.Items.Add(s.Id + ". " + s.Name + "(" + s.IndexNumber + ") - " +
+                    s.AverageMark);
+            }
+        }
+    }
+}
