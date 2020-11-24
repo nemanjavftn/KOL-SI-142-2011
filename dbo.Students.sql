b1b623ed263eadea6f86e5f4c073a57f47653157
Nemanja Vicovic <nemanja.vicovic@ftn.edu.rs>
2020-11-24 19:34:46 +0100

    Dodat StudentRepository i metode na Data access sloju

--- dbo.Students.sql
+++ dbo.Students.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[Students] (
     [Id]          INT             IDENTITY (1, 1) NOT NULL,
     [Name]        NVARCHAR (100)  NOT NULL,
-    [IndexNumber] NCHAR (20)      NOT NULL,
+    [IndexNumber] NVARCHAR (20)      NOT NULL,
     [AverageMark] DECIMAL (16, 2) NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC)
 );
