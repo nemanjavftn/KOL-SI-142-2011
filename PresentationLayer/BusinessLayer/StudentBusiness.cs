0689c9b92cc03a0a8056d2abf26a0eab63800568
Nemanja Vicovic <nemanja.vicovic@ftn.edu.rs>
2020-11-24 19:45:43 +0100

    Dodate metode u StudentBusiness

---
+++ PresentationLayer/BusinessLayer/StudentBusiness.cs
@@ -0,0 +1,40 @@
+﻿using DataAccessLayer;
+using DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class StudentBusiness
+    {
+        private readonly StudentRepository studentRepository;
+
+        public StudentBusiness()
+        {
+            this.studentRepository = new StudentRepository();
+        }
+
+        public List<Student> GetAllStudents()
+        {
+            return this.studentRepository.GetAllStudents();
+        }
+
+        public bool InsertStudent(Student s)
+        {
+            if (this.studentRepository.InsertStudent(s) > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public List<Student> GetStudentsGTAvgMark(decimal averageMark)
+        {
+            return this.studentRepository.GetAllStudents()
+                .Where(s => s.AverageMark > averageMark).ToList();
+        }
+    }
+}
