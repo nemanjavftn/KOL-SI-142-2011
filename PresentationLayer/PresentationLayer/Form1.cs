b0e1be420d0911a97ede0c89be1f546741abed6c
Nemanja Vicovic <nemanja.vicovic@ftn.edu.rs>
2020-12-08 19:54:06 +0100

    Added validation (regex example)

--- PresentationLayer/PresentationLayer/Form1.cs
+++ PresentationLayer/PresentationLayer/Form1.cs
@@ -8,6 +8,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -16,6 +17,7 @@ namespace PresentationLayer
     public partial class Form1 : Form
     {
         private readonly IStudentBusiness studentBusiness;
+        private string emailRegex = @"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$";
         public Form1(IStudentBusiness _studentBusiness)
         {
             this.studentBusiness = _studentBusiness;
@@ -41,21 +43,62 @@ namespace PresentationLayer
 
         private void buttonInsertStudent_Click(object sender, EventArgs e)
         {
-            Student s = new Student();
-            s.Name = textBoxName.Text;
-            s.IndexNumber = textBoxIndexNumber.Text;
-            s.AverageMark = Convert.ToDecimal(textBoxAverageMark.Text);
-
-            if (this.studentBusiness.InsertStudent(s))
+            if (!string.IsNullOrEmpty(textBoxName.Text) &&
+                !string.IsNullOrEmpty(textBoxIndexNumber.Text) &&
+                !string.IsNullOrEmpty(textBoxAverageMark.Text) &&
+                textBoxName.Text.Length <= 100 &&
+                Regex.Match(textBoxEmail.Text, emailRegex).Success)
             {
-                RefreshData();
-                textBoxName.Text = "";
-                textBoxIndexNumber.Text = "";
-                textBoxAverageMark.Text = "";
+                Student s = new Student();
+                s.Name = textBoxName.Text;
+                s.IndexNumber = textBoxIndexNumber.Text;
+                s.AverageMark = Convert.ToDecimal(textBoxAverageMark.Text);
+
+                if (this.studentBusiness.InsertStudent(s))
+                {
+                    RefreshData();
+                    textBoxName.Text = "";
+                    textBoxIndexNumber.Text = "";
+                    textBoxAverageMark.Text = "";
+                }
+                else
+                {
+                    MessageBox.Show("Greska!");
+                }
+            } else
+            {
+                // MessageBox.Show("Morate popuniti sva polja!");
+                label4.Text = "Nesto nije validno!";
             }
+                
+        }
+
+        private void textBoxName_TextChanged(object sender, EventArgs e)
+        {
+           //Validation();
+        }
+
+        private void textBoxIndexNumber_TextChanged(object sender, EventArgs e)
+        {
+            //Validation();
+        }
+
+        private void textBoxAverageMark_TextChanged(object sender, EventArgs e)
+        {
+            //Validation();
+        }
+
+        private void Validation()
+        {
+            if (!string.IsNullOrEmpty(textBoxName.Text) &&
+                !string.IsNullOrEmpty(textBoxIndexNumber.Text) &&
+                !string.IsNullOrEmpty(textBoxAverageMark.Text))
+            {
+                buttonInsertStudent.Enabled = true;
+            } 
             else
             {
-                MessageBox.Show("Greska!");
+                buttonInsertStudent.Enabled = false;
             }
         }
     }
